aeb5fd3be9b2bd2c59921af5c9b2c550c25de1a1
Rocio Caballero <drcaballero25@gmail.com>
2022-02-07 19:21:53 -0300

    React Dashboard para la escondida

---
+++ src/components/ContentRowMovies.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import SmallCard from './SmallCard';
+
+/*  Cada set de datos es un objeto literal */
+
+/* <!-- Movies in DB --> */
+
+let moviesInDB = {
+    title: 'Productos en Base de Datos',
+    color: 'primary', 
+    cuantity: 21,
+    icon: 'fa-clipboard-list'
+}
+
+/* <!-- Total awards --> */
+
+let totalAwards = {
+    title:' Total de Usuarios', 
+    color:'success', 
+    cuantity: '79',
+    icon:'fas fa-user'
+}
+
+/* <!-- Actors quantity --> */
+
+let actorsQuantity = {
+    title:'Categorias' ,
+    color:'warning',
+    cuantity:'49',
+    icon:'<fas fa-mouse-pointer'
+}
+
+let cartProps = [moviesInDB, totalAwards, actorsQuantity];
+
+function ContentRowMovies(){
+    return (
+    
+        <div className="row">
+            
+            {cartProps.map( (movie, i) => {
+
+                return <SmallCard {...movie} key={i}/>
+            
+            })}
+
+        </div>
+    )
+}
+
+export default ContentRowMovies;
